46d968c7bbd5dc9bc11d8fa26dd7c0e0e999fc69
Jiamiao_Li <jl11639@nyu.edu>
2024-02-14 22:57:46 -0500

    commit

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -10,12 +10,12 @@ import './AboutUs.css';
 const AboutUs = props => {
     return (
       <>
-        <h1>About Us</h1>
+        
         <p>"I'm Jiamiao Li, a Computer Science major at New York University. My journey in technology has been driven by a strong passion for understanding the intricacies of software engineering and an eagerness to explore the vast potential of data analysis. Outside the classroom, I immerse myself in various projects that allow me to apply theoretical knowledge to practical challenges. This not only shape my academic focus but also inspire my aspirations for future innovations.",
     imageUrl: "https://photos.app.goo.gl/bF183kwMdQ3i45AQ9"</p>
         
       </>
     )
     }
-    
+
 export default AboutUs;
