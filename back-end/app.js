66a4b58f45c8304e4edb76936bd4680dd146dc7e
Jiamiao_Li <jl11639@nyu.edu>
2024-02-14 22:38:38 -0500

    new commit

--- back-end/app.js
+++ back-end/app.js
@@ -78,14 +78,7 @@ app.post('/messages/save', async (req, res) => {
   }
 })
 
-// a new route to add "About Us"
-app.get('/about', async(req, res) => {
-  res.json({
-    title: "About Us",
-    content: "I'm Jiamiao Li, a Computer Science major at New York University. My journey in technology has been driven by a strong passion for understanding the intricacies of software engineering and an eagerness to explore the vast potential of data analysis. Outside the classroom, I immerse myself in various projects that allow me to apply theoretical knowledge to practical challenges. This not only shape my academic focus but also inspire my aspirations for future innovations.",
-    imageUrl: "https://photos.app.goo.gl/bF183kwMdQ3i45AQ9"
-  });
-});
+
 
 
 // export the express app we created to make it available to other modules
--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -5,22 +5,19 @@ import './AboutUs.css';
 
 
 
-function AboutUs() {
-  const [aboutInfo, setAboutInfo] = useState({ title: '', content: '', imageUrl: '' });
 
-  useEffect(() => {
-    fetch('/about')
-      .then(response => response.json())
-      .then(data => setAboutInfo(data));
-  }, []);
 
-  return (
-    <div>
-      <h1>{aboutInfo.title}</h1>
-      <p>{aboutInfo.content}</p>
-      <img src={aboutInfo.imageUrl} alt="About Us" />
-    </div>
-  );
-}
+const AboutUs = props => {
+    return (
+      <>
+        <h1>About Us</h1>
+        <p>"I'm Jiamiao Li, a Computer Science major at New York University. My journey in technology has been driven by a strong passion for understanding the intricacies of software engineering and an eagerness to explore the vast potential of data analysis. Outside the classroom, I immerse myself in various projects that allow me to apply theoretical knowledge to practical challenges. This not only shape my academic focus but also inspire my aspirations for future innovations.",
+    imageUrl: "https://photos.app.goo.gl/bF183kwMdQ3i45AQ9"</p>
+        <p>
+          Check out the <Link to="/messages">messages page</Link>.
+        </p>
+      </>
+    )
+    }
 
 export default AboutUs;
