fddc742b367f593d04d06875b41cd70472fd4bea
Jiamiao_Li <jl11639@nyu.edu>
2024-02-14 23:11:01 -0500

    commit

--- front-end/src/AboutUs.js
+++ front-end/src/AboutUs.js
@@ -5,7 +5,7 @@ const AboutUs = props => {
     return (
       <>
         
-        <p>"This is Jiamiao Li. I'm senior, majoring in Computer Science at New York University.</p>
+        <p>This is Jiamiao Li. I'm senior, majoring in Computer Science at New York University.</p>
         
       </>
     )
